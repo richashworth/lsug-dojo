dda228810170627f052885b2eda6f8a83ee51fce
Richard Ashworth <rich@Richards-MacBook-Pro.local>
2015-10-04 16:10:42 +0100

    extract message for invalid input

--- src/main/scala/lsug/LCDFormatter.scala
+++ src/main/scala/lsug/LCDFormatter.scala
@@ -1,6 +1,7 @@
 package lsug
 
 object LCDFormatter {
+
   val digitMapping = Map(
     0 -> zero,
     1 -> one,
@@ -13,15 +14,18 @@ object LCDFormatter {
     8 -> eight,
     9 -> nine)
 
+  val invalidInputMsg = s"${getClass.getName} only displays positive numbers"
+
   def merge(a: LCDDisplay, b: LCDDisplay): LCDDisplay = {
     LCDDisplay(a.firstRow  + " " + b.firstRow,
-             a.secondRow + " " + b.secondRow,
-             a.thirdRow  + " " + b.thirdRow)
+               a.secondRow + " " + b.secondRow,
+               a.thirdRow  + " " + b.thirdRow)
   }
 
   def parse(input: BigInt): LCDDisplay = (input compare 0).signum match {
-    case -1 => throw new IllegalArgumentException(s"${getClass.getName} only displays positive numbers")
-    case _  => input.toString().map(a => digitMapping(a.asDigit)).reduce((a, b) => merge(a, b))
+    case -1 => throw new IllegalArgumentException(invalidInputMsg)
+    case _ => input.toString().map(a => digitMapping(a.asDigit)).reduce((a, b) => merge(a, b))
+//    case _ => input.toString().par.map(a => digitMapping(a.asDigit)).reduce((a, b) => merge(a, b))
   }
 
   def format(input: BigInt) = parse(input).toString
